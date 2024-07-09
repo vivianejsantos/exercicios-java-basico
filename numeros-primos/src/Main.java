16039e9af68053e14c1f4f3cc86df25f350685bf
Viviane <viviane.jesussantos@hotmail.com>
2024-07-09 06:05:30 -0300

    commit initial

---
+++ numeros-primos/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numeroInicial, numeroFinal;
+        ArrayList<Integer> primos = new ArrayList<>();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre com número inicial");
+        numeroInicial= sc.nextInt();
+        System.out.println("Entre com número Final");
+        numeroFinal= sc.nextInt();
+
+        for(int i =1; i <= numeroFinal;i++){
+            if(ehprimo(i)==true){
+                primos.add(i);
+            }
+        }
+        System.out.println(primos);
+
+        }
+
+        private static boolean ehprimo(int n){
+        int contador = 0;
+        for(int i =1; i <=n; i++){
+            if(n%i==0){
+                contador+=1;
+            }
+        }if (contador==2)
+            return true;
+        else
+            return false;
+        }
+}
